b53e2d8dea9bbf5a3569a7a990ca8006473c51fb
gauravvinchurkar <gauravvinchurkar33@gmail.com>
2023-12-11 01:04:56 +0530

    commit subnet 2

--- project/modules/vpc/output.tf
+++ project/modules/vpc/output.tf
@@ -1,8 +1,8 @@
 output "pvt_subnet_id" {
-    value = aws.subnet.pvt_subnet_id
+    value = aws_subnet.pvt_subnet
 }
 output "pub_subnet_id" {
-    value = aws.subnet.pub_subnet_id
+    value = aws_subnet.pub_subnet
 }
 output "vpc_id" {
     value = aws_vpc.my_vpc.id
